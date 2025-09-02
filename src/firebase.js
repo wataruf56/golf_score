35751d19d0a676d5e9b96878c05e257b25a2d3cd
wataruf56 <da_is_on@yahoo.co.jp>
2025-09-02 18:03:35 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
 // あなたの Firebase プロジェクトの値に置き換えてください
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-      apiKey: "AIzaSyCj5DqageKdTk23HVpv2ktSLHyOdlq2ksI",
-      authDomain: "golf-score-app56.firebaseapp.com",
-      projectId: "golf-score-app56",
-      storageBucket: "golf-score-app56.firebasestorage.app",
-      messagingSenderId: "984454469289",
-      appId: "1:984454469289:web:5cdedf6eb40d0a50befbb8"
+  apiKey: "AIzaSyCj5DqageKdTk23HVpv2ktSLHyOdlq2ksI",
+  authDomain: "golf-score-app56.firebaseapp.com",
+  projectId: "golf-score-app56",
+  storageBucket: "golf-score-app56.appspot.com", // ← 修正
+  messagingSenderId: "984454469289",
+  appId: "1:984454469289:web:5cdedf6eb40d0a50befbb8",
 };
+
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 auth.languageCode = "ja";
 export const db = getFirestore(app);
+
+// デバッグ（一度だけ使う）: 実際に SDK が持っているキーを確認
+console.log("Firebase apiKey:", getApp().options.apiKey);
